8a73bc9e21c61e669006a8c837a8c4626296e1cd
Matheusmrb10 <44869160+Matheusmrb10@users.noreply.github.com>
2018-11-30 18:59:26 -0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE hmtl>
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<title> CRUD- CADASTRAR</title>
+	</head>
+	<body>
+		<h1>cadastar usuário</h1>
+
+		<form method="POST" action="processa.php"  > 
+
+
+
+		</form>
+
+	</body>
